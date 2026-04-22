acfa80910c8c4d005900201d9d5174d3cc597558
miltonninach <timontnch@gmail.com>
2026-04-21 21:24:38 -0400

    Base de datos SQLite creada con tabla pacientes

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
+def init_db():
+    conn = sqlite3.connect('citas.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS pacientes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            mascota TEXT NOT NULL,
+            propietario TEXT NOT NULL,
+            especie TEXT,
+            fecha TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
 
 if __name__ == '__main__':
     app.run(debug=True)
